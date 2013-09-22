ca0815ebdef5ca6f38d21a06563c382b2fb915d3
John Oliva <joliva63@gmail.com>
2013-09-22 18:41:03 -0400

    Completed test application using button widget.

--- lab2-315/ButtonGridTester/app/controllers/index.js
+++ lab2-315/ButtonGridTester/app/controllers/index.js
@@ -1,5 +1,17 @@
-function doClick(e) {
-    alert($.label.text);
-}
+$.buttongrid.init({
+    buttons: [
+        { id: 'write', title: "Notes", backgroundColor: 'gray'},
+        { id: 'lightbulb', title: "Ideas" },
+        { id: 'bubble', title: 'Chat' },
+        { id: 'person', title: "People" },
+        { id: 'pictures', title: 'Pictures', click: function (e) { alert("You got me!"); }}
+    ],
+    buttonWidth: Alloy.isTablet ? 200: 100,
+    buttonHeight: Alloy.isTablet ? 200 : 100,
+    backgroundColor: '#660000',
+    backgroundSelectedColor: '#A30000',
+    textSelectedColor: 'brightblue',
+    duration: 800
+});
 
 $.index.open();
