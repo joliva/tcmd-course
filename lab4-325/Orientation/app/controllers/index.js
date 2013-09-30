fcbe4b4ae5c550668bc459e6a02abf88c44616d7
John Oliva <joliva63@gmail.com>
2013-09-30 19:37:02 -0400

    Starting point for lab 325 on orientation and gestures.

---
+++ lab4-325/Orientation/app/controllers/index.js
@@ -0,0 +1,24 @@
+var images = [],
+	notes = [];
+
+notes[0] = 'Together Through Life is the 33rd studio album by Bob Dylan, released on April 28, 2009, on Columbia Records. The album debuted at number one in several countries, including the U.S. and the UK. It is Dylan\'s first number one in Britain since New Morning in 1970.';
+notes[1] = "Rumors of the album, reported in Rolling Stone magazine, came as a surprise, with no official press release until March 16, 2009 ‚Äî less than two months before the album's release date.";
+notes[2] = "In a conversation with music journalist Bill Flanagan, published on Bob Dylan's official website, Flanagan suggested a similarity of the new record to the sound of Chess Records and Sun Records, which Dylan acknowledged as an effect of 'the way the instruments were played.'";
+
+images[0] = '/dylan.png';
+images[1] = '/dylan1.png';
+images[2] = '/dylan2.png';
+
+// handle the swipe event -- change the liner notes 
+// to a random member of the notes array
+
+
+// handle the shake event -- change the cover art image
+// to a random member of the images array
+
+
+// handle the orientation change event
+// by moving elements to new locations
+
+
+$.index.open();
