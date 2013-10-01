3fbdd97aab8b2a4ab6dad0074ca4bf8df98e7828
John Oliva <joliva63@gmail.com>
2013-09-30 23:48:13 -0400

    Compensate for window under statusbar on iOS 7.

--- lab4-325/Orientation/app/alloy.js
+++ lab4-325/Orientation/app/alloy.js
@@ -9,3 +9,35 @@
 // object. For example:
 //
 // Alloy.Globals.someGlobalFunction = function(){};
+
+if (OS_IOS) {
+	// Function to test if device is iOS 7 or later
+	function isIOS7Plus() {
+		// iOS-specific test
+		if (Titanium.Platform.name == 'iPhone OS') {
+			var version = Titanium.Platform.version.split(".");
+			var major = parseInt(version[0],10);
+
+			// Can only test this support on a 3.2+ device
+			if (major >= 7)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	if (isIOS7Plus() == true) {
+		Alloy.CFG.isIOS7Plus = true;
+		
+		var statusBarHeight = 20;	// from IOS documentation
+		Alloy.CFG.winTopOffset = statusBarHeight;		
+	} else {
+		Alloy.CFG.isIOS7Plus = false;
+		Alloy.CFG.winTopOffset = 0;
+	}
+	
+	Ti.UI.setBackgroundColor('#dcdcdc');	// used to set window default background color
+} else {
+	Alloy.CFG.winTopOffset = 0;
+}
