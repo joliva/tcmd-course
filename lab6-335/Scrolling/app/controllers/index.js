05c926f235076be0a5e19fe142ccebd3329f3d1b
John Oliva <joliva63@gmail.com>
2013-10-01 18:00:58 -0400

    Completed exercise.

--- lab6-335/Scrolling/app/controllers/index.js
+++ lab6-335/Scrolling/app/controllers/index.js
@@ -1,5 +1,21 @@
-function doClick(e) {
-    alert($.label.text);
+var prevPlayer = $.player1;
+prevPlayer.color = 'red';
+
+function setPlayer(e) {
+	var curPlayer = e.source;
+	var player = curPlayer.player;
+	
+	if (curPlayer != prevPlayer) {
+		prevPlayer.color = 'black';
+		prevPlayer = curPlayer;
+		curPlayer.color = 'red';
+	}
+	
+	if (player != null) {		
+		for (var i=1; i<4; i++) {
+			$['image' + i].image = '/' + player + i + '.jpg';
+		}
+ 	}
 }
 
 $.index.open();
