49e60d8ef633512ddc1037461fbb132c58786b8e
John Oliva <joliva63@gmail.com>
2013-10-03 09:50:50 -0400

    Completed exercise.

--- lab8-350/Android Deep Dive/app/controllers/index.js
+++ lab8-350/Android Deep Dive/app/controllers/index.js
@@ -1,2 +1,25 @@
+function shareViaIntent() {
+	// hide keyboard
+	Ti.UI.Android.hideSoftKeyboard();
+	
+	// the type parameter defines the mime type of the data to be shared
+    var intent = Ti.Android.createIntent({
+        action: Ti.Android.ACTION_SEND,
+        type: 'text/plain'
+    });
+    //Android developer docs provide info on required and optional extra data fields with intents
+    intent.putExtra(Ti.Android.EXTRA_SUBJECT, 'Sharing via Intent');
+    intent.putExtra(Ti.Android.EXTRA_TEXT, $.text.value);
+    try {
+        // Start the activity and pass the intent object
+        Ti.Android.currentActivity.startActivity(intent);
+    } catch (ex) {
+        // Handle Exception if no suitable apps installed
+        Ti.UI.createNotification({
+        	message: 'No sharing apps installed!',
+        	duration: Ti.UI.NOTIFICATION_DURATION_LONG
+        }).show();
+    }
+}
 
 $.index.open();
