25ab82df6b6a69180568ddc9c7509c1d84a2b862
John Oliva <joliva63@gmail.com>
2013-10-05 20:56:11 -0400

    Completed exercise.

--- lab10-360/Settings/app/controllers/index.js
+++ lab10-360/Settings/app/controllers/index.js
@@ -1,2 +1,21 @@
+function setLayout() {
+	var val = Ti.App.Properties.getBool('rtl_preference');
+	if (val) {
+		$.fugitiveImage.left = null;
+		$.fugitiveImage.right = 10;
+		$.vitalsView.left = 10;
+		$.vitalsView.right = 110;
+	} else {
+		$.fugitiveImage.left = 10;
+		$.fugitiveImage.right = null;
+		$.vitalsView.left = 110;
+		$.vitalsView.right = 10;
+	}
+}
 
+Ti.App.addEventListener('resume', function() {
+	setLayout();
+});
+
+setLayout();
 $.index.open();
