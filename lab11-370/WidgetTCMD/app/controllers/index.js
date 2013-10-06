5a673e6c87bf7b5003be9f9ef08a495da783c990
John Oliva <joliva63@gmail.com>
2013-10-06 09:37:13 -0400

    Completed exercise.

--- lab11-370/WidgetTCMD/app/controllers/index.js
+++ lab11-370/WidgetTCMD/app/controllers/index.js
@@ -1,2 +1,14 @@
 
+/**
+ * In this example, you'll learn how to use the PayPal module.
+ *
+ * There are four different types of payments you can do:
+ * - Simple Payments
+ * - Parallel Payments
+ * - Chain Payments
+ * - and Preapprovals
+ *
+ * We'll demonstrate each of these in their own tab.
+ */
+
 $.index.open();
